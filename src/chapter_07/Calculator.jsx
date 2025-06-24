28a984a8d4ce794e11cdae88314219508882d268
shinis(ys) <bigpoby@naver.com>
2025-06-24 11:15:46 +0900

    화면 구성

--- src/chapter_07/Calculator.jsx
+++ src/chapter_07/Calculator.jsx
@@ -1,3 +1,10 @@
 export default function Calculator() {
-    return <div>Calculator</div>
+    return (
+        <div>
+            <input type="text" />
+            <input type="text" />
+            <button>첫번째 입력에 포커스</button>
+            <div>결과(제곱의 합): </div>
+        </div>
+    )
 }
